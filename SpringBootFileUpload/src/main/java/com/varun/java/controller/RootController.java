8f2af3766edd5222dc7103b5601069bf28c03bae
varun krishna vankayalapati <vrvarunkrishna@gmail.com>
2017-09-17 21:25:47 -0400

    Initial Commit to Project

---
+++ SpringBootFileUpload/src/main/java/com/varun/java/controller/RootController.java
@@ -0,0 +1,14 @@
+package com.varun.java.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class RootController {
+
+    @GetMapping("/")
+    public String index() {
+        return "upload";
+    }
+
+}
---
+++ SpringBootFileUploadOp/spring-boot-file-upload-ajax-rest/src/main/java/com/varun/java/SpringBootRestfulApp.java
@@ -0,0 +1,16 @@
+package com.varun.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestfulApp {
+
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(SpringBootRestfulApp.class, args);
+    }
+
+    
+
+}
